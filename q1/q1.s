d57a66bbf2954cb23b34ae9acdde53604fde4557
rishima31 <rishimarajput31@gmail.com>
2026-04-04 10:59:53 +0000

    q1.s added

---
+++ q1/q1.s
@@ -0,0 +1,191 @@
+.global make_node
+.global insert
+.global get
+.global getAtMost
+
+make_node:
+    addi sp,sp,-16
+    sd ra,8(sp)
+
+    mv t0,a0                # t0 = val
+
+    li a0,24
+    call malloc             # a0 = pointer to new node
+
+    sw t0,0(a0)             # store val 
+    sd x0,8(a0)             # left = NULL
+    sd x0,16(a0)            # right = NULL
+
+    ld ra,8(sp)
+    addi sp,sp,16
+    ret
+
+###########################################
+
+insert:
+    addi sp,sp,-32
+    sd ra,24(sp)            # save return address
+    sd a0,16(sp)            # save root
+    sd a1,8(sp)             # save val
+
+    beq a0,x0,call_m_n      # if root == NULL, create new node
+
+    lw t0,0(a0)             # t0 = root->val
+
+    blt a1,t0,go_left       # val < root->val → go left
+    bgt a1,t0,go_right      # val > root->val → go right
+
+    ld ra,24(sp)
+    addi sp,sp,32
+    ret
+
+call_m_n:
+    ld a0,8(sp)             # a0 = val
+    call make_node          # returns new node in a0
+
+    ld ra,24(sp)
+    addi sp,sp,32
+    ret
+
+go_left:
+    ld a0,8(a0)             # a0 = root->left
+    ld a1,8(sp)             # a1 = val
+    call insert             # returns updated left subtree
+
+    ld t1,16(sp)            # t1 = root (restored from stack)
+    sd a0,8(t1)             # root->left = returned node
+
+    mv a0,t1                # return root
+    ld ra,24(sp)
+    addi sp,sp,32
+    ret
+
+go_right:
+    ld a0,16(a0)            # a0 = root->right
+    ld a1,8(sp)             # a1 = val
+    call insert             # returns updated right subtree
+
+    ld t1,16(sp)            # t1 = root (restored from stack)
+    sd a0,16(t1)            # root->right = returned node
+
+    mv a0,t1                # return root
+    ld ra,24(sp)
+    addi sp,sp,32
+    ret
+
+###########################################
+
+get:   
+    addi sp,sp,-24
+    sd ra,16(sp)            # save return address
+    sd a0,8(sp)             # save root
+    sd a1,0(sp)             # save val
+
+    beq a0,x0,end_get       # if root == NULL, end
+
+    lw t0,0(a0)             # t0 = root->val
+
+    beq a1,t0,found         # if root->val==val, go to found
+
+    blt a1,t0,find_left     # if val < root->val go left
+    bgt a1,t0,find_right    # if val > root->val go right
+
+    ld ra,16(sp)
+    addi sp,sp,24
+    ret
+
+end_get:
+    mv a0,x0                # return NULL (0)
+    ld ra,16(sp)            # a0 already has root value so just return that
+    addi sp,sp,24
+    ret
+
+
+found:
+    ld ra,16(sp)            # a0 already has root value so just return that
+    addi sp,sp,24
+    ret
+
+find_left:
+    ld a0,8(a0)             # a0 = root->left
+    ld a1,0(sp)             # a1 = val
+
+    call get
+
+    ld ra,16(sp)
+    addi sp,sp,24
+    ret
+
+find_right:
+    ld a0,16(a0)            # a0 = root->right
+    ld a1,0(sp)             # a1 = val
+
+    call get
+
+    ld ra,16(sp)
+    addi sp,sp,24
+    ret
+
+###########################################
+
+getAtMost:
+    addi sp,sp,-32
+    sd ra,24(sp)            # save return address
+    sd a0,16(sp)            # save val
+
+    beq a1,x0,end           # if root==NULL, end
+
+    sd a1,8(sp)             # save a1 = root (current node)
+
+loop:
+    ld t0,8(sp)             # load curr
+    ld a0,16(sp)            # a0 = val
+
+    beq t0,x0,check_end     # if curr==NULL, go to check_end
+
+    lw t1,0(t0)             # t1 = root->val
+
+    bgt a0,t1,right         # if val > root->val go right
+    blt a0,t1,left          # if val < root->val go left
+
+    beq a0,t1,pred          # if val==root->val go to pred
+
+check_end:
+    ld t0,0(sp)             # load value of predecessor
+
+    beq t0,x0,pred_end      # if t0==NULL, end
+    lw t1,0(t0)             # t1 = predecessor->val
+
+    mv a0,t1                # a0 = t1
+    ld ra,24(sp)
+    addi sp,sp,32
+    ret
+
+pred_end:
+    li a0,-1                # return -1
+    ld ra,24(sp)
+    addi sp,sp,32
+    ret
+
+right:
+    ld t0,8(sp)             # load curr
+    sd t0,0(sp)             # save curr as predecessor
+
+    ld t1,16(t0)            # t1 = curr->right
+    sd t1,8(sp)             # update curr to curr->right
+
+    j loop                  # run loop again
+
+left:
+    ld t0,8(sp)             # load curr
+
+    ld t1,8(t0)             # t1 = curr->left
+    sd t1,8(sp)             # update curr to curr->left
+
+    j loop                  # run loop again
+
+pred:
+    ld t0,8(sp)             # load curr
+    sd t0,0(sp)             # save curr as predecessor
+
+    j check_end
